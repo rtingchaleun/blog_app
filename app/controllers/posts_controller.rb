c9269321905c22f651f2c7cbdd4c78ee53d15164
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2020-03-23 17:01:46 -0400

    Added delete functions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,14 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
+    console
     @posts = Post.all
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    console
   end
 
   # GET /posts/new
@@ -28,7 +30,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path, notice: "'#{@post.title}' was created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -42,7 +44,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post, notice: "'#{@post.title}' was updated." }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -56,7 +58,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: "'#{@post.title}' was deleted." }
       format.json { head :no_content }
     end
   end
