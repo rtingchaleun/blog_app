ddcd0cf5f7420563f8d0e1264ceb79af5b7b423a
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2020-03-26 12:30:16 -0400

    added user references to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_145649) do
+ActiveRecord::Schema.define(version: 2020_03_26_162135) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -48,7 +48,9 @@ ActiveRecord::Schema.define(version: 2020_03_26_145649) do
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -76,5 +78,6 @@ ActiveRecord::Schema.define(version: 2020_03_26_145649) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -37,5 +37,6 @@ end
   Comment.create(
     body: Faker::Lorem.sentence,
     post_id: 1,
+    user_id: 1,
   )
 end
