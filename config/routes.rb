518d7c5af8928811c9e47c601f21a3deedb4cbf2
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2020-03-29 15:38:12 -0400

    Scaffolded Categories. Added categories reference to posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'posts#index'
+
   get 'my_posts' => 'posts#my_posts'
+
+  devise_for :users
 
   resources :posts do
     resources :comments
   end
 
-  devise_for :users
+  resources :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_162135) do
+ActiveRecord::Schema.define(version: 2020_03_29_193345) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2020_03_26_162135) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id", null: false
@@ -60,6 +66,8 @@ ActiveRecord::Schema.define(version: 2020_03_26_162135) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "views", default: 0
     t.integer "user_id"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -79,5 +87,6 @@ ActiveRecord::Schema.define(version: 2020_03_26_162135) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
 end
